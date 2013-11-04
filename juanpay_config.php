121a950328ef46ad9c1d2a9ecfca85f4e4949806
Chris Ian Fiel <ccfiel@gmail.com>
2013-11-04 19:03:21 +0800

    update config

--- juanpay_config.php
+++ juanpay_config.php
@@ -1,8 +1,8 @@
 <?
  define("DEBUG", 1);
  define("USE_SANDBOX", 1);
- define('API_KEY', 'e4fd617bfbce1a75fc9223a27950c130');
- define('JUANPAY_ACCOUNT_EMAIL', 'rtfiel@juanpay.ph');
+ define('API_KEY', '6d2a252fbc221d087e1aa75e7182f3f5');
+ define('JUANPAY_ACCOUNT_EMAIL', 'rtfiel@agile.com.ph');
  define("LOG_FILE", "/tmp/dpn.log");
 
  if (USE_SANDBOX==1) {
